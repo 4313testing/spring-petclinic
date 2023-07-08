01db4010f06a6059e0ca22fed9cc5e6afd1fe609
bijomutta <bijomutta@gmail.com>
2023-07-08 23:26:20 +0200

    #Bug1: Setting dates doesn't work properly - they are shifted by one day bug

---
+++ src/main/java/org/springframework/samples/petclinic/owner/test.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.springframework.util.Assert;
+
+import java.util.*;
+
+public class test {
+
+	public static void main (String [] args)
+	{
+		List<String> strings=new ArrayList<>();
+		int count=0;
+		for (int i=0;i<10;i++)
+		{
+
+			count++;
+			System.out.println(count);
+
+		}
+	}
+}
